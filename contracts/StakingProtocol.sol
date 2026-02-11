2262e74a81a68a1a98f50d9c9dbe7a520a200d00
Antonio Makaronio <manueomlestadga@hotmail.com>
2026-02-11 15:59:35 +0200

    Update StakingProtocol.sol

--- contracts/StakingProtocol.sol
+++ contracts/StakingProtocol.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";  
-
-
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract StakingProtocol is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
@@ -15,19 +15,21 @@ contract StakingProtocol is Ownable, ReentrancyGuard {
     mapping(address => uint256) public staked;
 
     event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
 
     constructor(address _stakeToken) Ownable(msg.sender) {
         require(_stakeToken != address(0), "zero");
         stakeToken = IERC20(_stakeToken);
     }
 
-    function stake(uint256 amount) public nonReentrant { 
+    function stake(uint256 amount) public nonReentrant {
         require(amount > 0, "amount=0");
         stakeToken.safeTransferFrom(msg.sender, address(this), amount);
         staked[msg.sender] += amount;
-        emit Staked(msg.sender, amount); 
+        emit Staked(msg.sender, amount);
     }
 
+    // Improvement: permit-based staking
     function stakeWithPermit(
         uint256 amount,
         uint256 deadline,
@@ -37,5 +39,12 @@ contract StakingProtocol is Ownable, ReentrancyGuard {
     ) external {
         IERC20Permit(address(stakeToken)).permit(msg.sender, address(this), amount, deadline, v, r, s);
         stake(amount);
+    }
+
+    function unstake(uint256 amount) external nonReentrant {
+        require(amount > 0 && staked[msg.sender] >= amount, "bad amount");
+        staked[msg.sender] -= amount;
+        stakeToken.safeTransfer(msg.sender, amount);
+        emit Unstaked(msg.sender, amount);
     }
 }
