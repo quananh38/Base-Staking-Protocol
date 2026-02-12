307c5a107447b23a34af42f83503feafd941a25a
Antonio Makaronio <manueomlestadga@hotmail.com>
2026-02-12 16:37:40 +0200

    Update StakingProtocol.sol

--- contracts/StakingProtocol.sol
+++ contracts/StakingProtocol.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+
 
 contract StakingProtocol is Ownable, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
